f335a733aa6549a4a7e23d412b95193a02e3d808
Andrii Dehtiarov <adehtiarov@amcbridge.com>
2025-01-11 00:27:11 +0200

    Fix compilation error in example from 9th slide

--- 9_launching_the_thread.cpp
+++ 9_launching_the_thread.cpp
@@ -1,26 +1,27 @@
 #include <iostream>
 #include <thread>
 
+using namespace std::chrono_literals;
+
 class background_task
 {
 public:
     void operator()() const
     {
-        do_something();
-        do_something_else();
+        std::this_thread::sleep_for(5s);
     }
 };
 
 int main() {
     // Create a thread object and launch the thread
     background_task f;
-    std::thread my_thread{f()}
+    std::thread my_thread{f};
     // C++ most vexing parse:
     // std::thread my_thread(f())
     // Either don't use temporary object or use uniform initialization syntax
 
     // Wait for the thread to finish
-    myThread.join();
+    my_thread.join();
 
     std::cout << "Thread has finished." << std::endl;
 
